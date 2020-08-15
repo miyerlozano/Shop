d8e5690fc47a42073ab9ddaa1de06aed9dc4a093
Miyer Lozano <miyerlozano@hotmail.com>
2020-08-14 23:51:40 -0500

    API

---
+++ Shop/Shop.web/Controllers/API/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.web.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.web.Controllers.API
+{
+    [Route("api/[Controller]")]
+
+	public class ProductsController : Controller
+	{
+		private readonly IProductRepository productRepository;
+
+		public ProductsController(IProductRepository productRepository)
+		{
+			this.productRepository = productRepository;
+		}
+
+		[HttpGet]
+		public IActionResult GetProducts()
+		{
+			return this.Ok(this.productRepository.GetAll());
+		}
+	}
+
+
+}
---
+++ Shop/Shop.web/Models/ProductViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Shop.web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.web.Models
+{
+    public class ProductViewModel: Product
+    {
+        [Display(Name = "Image")]
+        public IFormFile ImageFile { get; set; }
+
+
+    }
+}
