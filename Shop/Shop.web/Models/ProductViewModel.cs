53803c0af2bb93d4e2997c0c42c3284d2e2c8c41
Miyer Lozano <miyerlozano@hotmail.com>
2020-08-15 00:32:08 -0500

    Create Image

--- Shop/Shop.web/Models/ProductViewModel.cs
+++ Shop/Shop.web/Models/ProductViewModel.cs
@@ -1,11 +1,12 @@
-﻿using Shop.web.Data.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
 
 namespace Shop.web.Models
 {
+    using System.ComponentModel.DataAnnotations;
+    using Data.Entities;
+    using Microsoft.AspNetCore.Http;
+
+
     public class ProductViewModel: Product
     {
         [Display(Name = "Image")]
